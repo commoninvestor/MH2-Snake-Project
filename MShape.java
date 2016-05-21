99ba2214efdcda9008a5ba31031ce8b3501c25a8
commoninvestor <jethromak0@gmail.com>
2016-05-21 18:39:27 -0400

    Update MShape.java

--- MShape.java
+++ MShape.java
@@ -8,6 +8,22 @@ public class MShape extends Shape {
     private int dx = 10;
     private int dy = 10;
 
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
     public void slide(int direction) {
         erase();
         if (direction == 1) {
