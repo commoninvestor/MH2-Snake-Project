ccb368f2f061e784383944310d5d8fff2c2ee275
S-Li <xsusanlie@hotmail.com>
2016-05-21 18:39:24 -0400

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -7,6 +7,25 @@ import java.awt.*;
  */
 public class Wall extends Shape{
 
+    protected int xCorner = 100;
+    protected int yCorner = 100;
+
+    public int getxCorner() {
+        return xCorner;
+    }
+
+    public void setxCorner(int xCorner) {
+        this.xCorner = xCorner;
+    }
+
+    public int getyCorner() {
+        return yCorner;
+    }
+
+    public void setyCorner(int yCorner) {
+        this.yCorner = yCorner;
+    }
+
     public void draw (Graphics g) {
         g.fillRect (xCorner, yCorner, Width, Height);
     }
