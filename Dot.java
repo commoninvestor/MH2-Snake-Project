d2db972d8792d58dd93aaa53144069f38fd85f92
commoninvestor <jethromak0@gmail.com>
2016-05-21 22:27:29 -0400

    Add files via upload

---
+++ Dot.java
@@ -0,0 +1,15 @@
+package Shapes;
+
+        import java.awt.*;
+
+/**
+ * Created by Jethro on 2016-05-21.
+ */
+public class Dot extends Shape{
+
+    public void draw (Graphics g){
+        g.fillOval (xCentre, yCentre, Width, Height)
+    }
+
+
+}
